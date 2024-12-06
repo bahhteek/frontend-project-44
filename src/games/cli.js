3267aa76b40de20a58a73a6e43d71b60b2b5d541
1manaliyev <baxtik.kkp@mail.ru>
2024-12-06 13:51:02 +0500

    fixed text

--- src/games/cli.js
+++ src/games/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const startBarinGames = () => {
   console.log('Welcome to the Brain Games!');
 
-  const name = readlineSync.question('May i have your name? ');
+  const name = readlineSync.question('May I have your name? ');
 
   console.log(`Hello, ${name}!`);
 };
